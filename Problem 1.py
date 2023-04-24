03ad800863657c831ad40ef93c3a854ca1ec00bc
Himesh <121535663+python266@users.noreply.github.com>
2023-04-24 18:23:48 +0530

    Add files via upload

---
+++ Problem 1.py
@@ -0,0 +1,12 @@
+def function():
+
+    a = int(input("Enter the first number: "))
+    b= int(input("Enter the secound number: "))
+
+    x = a * b
+    if x >= 1000:
+        print(x)
+    else:
+        print(a+b)
+
+function()
