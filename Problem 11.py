fa9ca5c51dc56c7a4a2329530d07db966856b65f
Himesh <121535663+python266@users.noreply.github.com>
2023-04-29 22:41:15 +0530

    Add files via upload

---
+++ Problem 11.py
@@ -0,0 +1,13 @@
+#Main code
+#Foumula = if a[i] >= b[i] then set a[i] = a[i]-b[i]
+ip1 = input()
+ip2 = input()
+
+changin1 = ip1.split(' ')
+changin2 = ip2.split(' ')
+
+list1 = [int(x) for x in changin1]
+list2 = [int(j) for j in changin2]
+
+print(list1)
+print(list2)
