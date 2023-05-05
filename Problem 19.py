a5b4daa0c7344bad055a16dd335b66caf2134d04
Himesh <121535663+python266@users.noreply.github.com>
2023-05-06 00:02:18 +0530

    Add files via upload

---
+++ Problem 19.py
@@ -0,0 +1,10 @@
+print("\t\tThis program print vowels in a string")
+i = input("Enter the string: ")
+
+x = i.count('a')
+a = i.count('e')
+r = i.count('i')
+d = i.count('o')
+y = i.count('u')
+
+print(f"A is {x}.\nE is {a}\nI is {r}\nO is {d}\nU is {y}")
