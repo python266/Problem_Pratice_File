9fc458b71ed0619762b2fe1612d7b2d06b7c2583
Himesh <121535663+python266@users.noreply.github.com>
2023-06-08 22:08:57 +0530

    Update Problem 52.py

--- Problem 52.py
+++ Problem 52.py
@@ -4,7 +4,7 @@ import pyttsx3
 def News():
     a = {"Name: ": "Top 100 News",
          "Sort by: ": "Top"}
-    url = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-08&sortBy=publishedAt&apiKey=5011f16d870e4d9283917dc862c65318"
+    url = "Here Fill Your API"
 
     b = requests.get(url)
     news = b.json()
